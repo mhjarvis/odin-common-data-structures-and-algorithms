88eb1bafb0712f3f17dec9328e3e7fb7fd1617ae
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-25 14:32:17 -0500

    update sizing and layout  of chess board visual

--- knights.js
+++ knights.js
@@ -1,11 +1,13 @@
 
 let gameBoard = []
-index = 0;
-
 
 for (let i = 1; i < 9; i++) {
     for (let j = 1; j < 9; j++) {
-        gameBoard.push([i, j])
+        const squareObj = {
+            x: 1,
+            y: j,
+        }
+        gameBoard.push(squareObj);
     }
 
 }
@@ -19,10 +21,50 @@ for (let i = 1; i < 9; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         div.id = 's' + i;
-        div.innerHTML = gameBoard[i];
+        div.innerHTML = gameBoard[i].x + ", " + gameBoard[i].y;
         getBoardElement.appendChild(div);
     }
 
-
-
 }())
+
+let knight = {
+    name: 'Knight',
+    moves: [
+        [1, 2], 
+        [1, -2], 
+        [-1, 2], 
+        [-1, -2], 
+        [2, 1], 
+        [2, -1], 
+        [-2, 1], 
+        [-2, -1]
+    ]
+}
+
+function knightMoves(start, end) {
+
+    if(start[0] == end[0] && start[1] == end[1]) 
+        return 'No moves neccessary'
+
+    let x = start[0];
+    let y = start[1];
+
+    let arr = [];
+
+    for(let i = 0; i < knight.moves.length; i++) {
+        let newArr = [];
+        newArr.push(x + knight.moves[i][0]);
+        newArr.push(y + knight.moves[i][1]);
+        arr.push(newArr);
+    }
+
+    console.log(arr[0])
+    return arr;
+
+}
+
+
+
+
+
+console.log(knightMoves([3, 3], [3, 4]))
