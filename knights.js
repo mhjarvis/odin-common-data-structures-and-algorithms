e8c0364330467863e92d7f905272fd674d591093
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-25 11:50:25 -0500

    add gameboard to page

--- knights.js
+++ knights.js
@@ -1,1 +1,27 @@
-console.log('test')
+
+let gameBoard = []
+index = 0;
+
+
+for (let i = 1; i < 9; i++) {
+    for (let j = 1; j < 9; j++) {
+        gameBoard.push([i, j])
+    }
+
+}
+
+// create gameboard and display
+(function () {
+    
+    const getBoardElement = document.querySelector('.gameboard');
+
+    for(let i = 0; i < gameBoard.length; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        div.id = 's' + i;
+        getBoardElement.appendChild(div);
+    }
+
+
+
+}())
